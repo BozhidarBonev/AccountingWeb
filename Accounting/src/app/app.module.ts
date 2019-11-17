580f50c11fe682751648ffde58a8abe5b7b619e9
AtanasDimulski <43001169+AtanasDimulski@users.noreply.github.com>
2019-11-18 00:10:55 +0100

    send email

--- Accounting/src/app/app.module.ts
+++ Accounting/src/app/app.module.ts
@@ -9,15 +9,15 @@ import { HomeComponent } from './home/home.component';
 import { AccountingServicesComponent } from './accounting-services/accounting-services.component';
 /*ANGULAR-MARTERIAL*/
 import {MatToolbarModule} from '@angular/material/toolbar';
-import{MatCardModule} from '@angular/material';
+import {MatCardModule} from '@angular/material';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material';
 /*ADDITIONAL*/
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ReactiveFormsModule } from '@angular/forms';
-import{BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { HttpClientModule }    from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
 
 
@@ -44,8 +44,8 @@ import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
     MatFormFieldModule,
     FormsModule,
     MatInputModule
-    
-  ],
+
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ Accounting/src/app/contact/user.ts
@@ -0,0 +1,10 @@
+export class User {
+  constructor(
+      public name: string,
+
+      public phone:string,
+      public message: string,
+      public email:string
+
+  ) {}
+}
